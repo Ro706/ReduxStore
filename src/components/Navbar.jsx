cd03b62d36aa2a2c3a70b770684912f3c4a5217b
Ro706 <rohitmandal99091@gmail.com>
2025-07-27 13:14:39 +0530

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
